41e6b1a0b3db5f6cb3a9efba110cb97416bff4d9
rahulbelwal90 <rahulbelwal90@gmail.com>
2025-10-01 14:16:07 +0530

    POST PUT PATCH request

--- Express/Day-1/index.js
+++ Express/Day-1/index.js
@@ -3,6 +3,7 @@ import data from "./data/data.js"
 
 const app = express();
 const PORT = 8080;
+app.use(express.json())
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello world");
@@ -34,11 +35,87 @@ app.get("/api/v1/users/:id", (req,res)=>{
     const user = data.find((user)=> user.id === parseId)
     res.status(200).send(user)
     }
-  }
+  } 
 )
 
+//* @ POST Request : It is for sending data to server
+
+app.post("/api/v1/users", (req,res)=>{
+  console.log(req.body);
+  const {name, displayname} = req.body
+
+  const newUser = {
+    id:data.length + 1,
+    name,
+    displayname
+  }
+
+  data.push(newUser)
+
+  res.status(201).send(
+    {
+      message:"User Created",
+      data:newUser
+    }
+  )
+})
+
+//* PUT Request (UPDATE all field)
+
+app.put("/api/v1/users/:id", (req,res)=>{
+  const {
+    body,
+    params: {id},
+  } = req;
+
+  const parseID = parseInt(id);
+  const userIndex = data.findIndex((user) => user.id === parseID);
+
+  if(userIndex === -1){
+    res.status(404).send("User Not Found");
+  }
+
+  data[userIndex] = {
+    id: parseID,
+    ...body
+  }
+
+  res.status(201).send(
+    {
+      message: "user updated",
+      data: data[userIndex]
+    }
+  )
+})
 
 
+//* PATCH Request ( Update specific feild)
+
+app.patch("/api/v1/users/:id", (req,res)=>{
+  const {
+    params:{id},
+    body
+  } = req;
+
+  const parseID = parseInt(id);
+  const userIndex = data.findIndex((user)=> user.id === parseID);
+
+  if(userIndex === -1){
+    res.status(404).send("User not found");
+  }
+
+  data[userIndex] = {
+    ...data[userIndex],
+    ...body
+  }
+  
+  res.status(201).send(
+    {
+      message: "User",
+      data: data[userIndex]
+    }
+  )
+})
 
 
 app.listen(PORT, (req, res) => {
